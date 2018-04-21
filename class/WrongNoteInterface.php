d056b854f39074644cd9ac7f26408f746f2baec7
King <782561742@qq.com>
2018-04-21 16:09:07 +0800

    ykx

---
+++ class/WrongNoteInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * @authors Your Name (you@example.org)
+ * @date    2018-04-21 15:57:07
+ * @version $Id$
+ */
+
+interface WrongNoteInterface {
+
+	public function statistics();
+
+	//public function select();
+	//public function where();
+}
